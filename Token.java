e53ef6477b43d0ab06659c032d6572edd3bab2d0
jmitchell59 <47366962+jmitchell59@users.noreply.github.com>
2022-12-08 13:10:48 -0500

    Add files via upload

---
+++ Token.java
@@ -0,0 +1,8 @@
+public class Token{
+string lexeme;  // lexeme representation
+    int tokenCode;  // token code
+
+    // Constructor
+    Token(string lex, int code) : lexeme(lex), tokenCode(code) {
+	}	
+}
